276fa3a62dbad4caa7a2d4fb665e5fc711c0f77f
Alexandre <abouttier@linaia.com>
2020-11-19 18:50:26 +0100

    fix error message et path

--- app.js
+++ app.js
@@ -4,59 +4,60 @@ const _ = require('lodash');
 const download = require('image-downloader');
 const config = require('./config.json');
 const fs = require('fs');
-cmlog.start('Start generator');
-
+const folderPatch = process.cwd();
 const cryptos = [];
 const filesErrors = [];
 
-axios
-  .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
-    headers: { 'X-CMC_PRO_API_KEY': config.apikey },
-  })
-  .then((res) => {
-    res.data.data.map((crypto, index) => {
-      cryptos.push(_.lowerCase(crypto.symbol));
+cmlog.start('Start generator');
+if (!_.isEmpty(config.apikey)) {
+  axios
+    .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+      headers: { 'X-CMC_PRO_API_KEY': config.apikey },
+    })
+    .then((res) => {
+      res.data.data.map((crypto, index) => {
+        cryptos.push(_.lowerCase(crypto.symbol));
+      });
+    })
+    .then(() => {
+      cmlog.success(
+        `Retrieving the list of cryptos Total: [${cryptos.length}]`
+      );
+
+      cryptos.forEach((crypto, index) => {
+        setTimeout(() => {
+          axios
+            .get(
+              `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${crypto}`,
+              {
+                headers: { 'X-CMC_PRO_API_KEY': config.apikey },
+              }
+            )
+            .then((o) => {
+              const imageUrl = o.data.data[_.upperCase(crypto)].logo;
+              const options = {
+                url: imageUrl,
+                dest: `${folderPatch}/icons/${crypto}.png`,
+              };
+
+              download
+                .image(options)
+                .then(({ filename }) => {
+                  cmlog.success(`Icon saved ${filename}`);
+                  cmlog.waitting(`PROGRESS [${index + 1}/${cryptos.length}]`);
+                })
+                .catch((err) => cmlog.error(new Error(err)));
+            })
+            .catch((err) => cmlog.error(new Error(err)));
+          if (index + 1 === cryptos.length) {
+            cmlog.done('All icons have been updated !');
+          }
+        }, 10000 * (index + 1));
+      });
+    })
+    .catch((err) => {
+      cmlog.error(new Error(err));
     });
-
-    try {
-      fs.writeFileSync('cryptos.txt', cryptos);
-    } catch (e) {
-      console.log('Cannot write file ', e);
-    }
-  })
-  .then(() => {
-    cmlog.success(
-      `Récuperation de la liste des cryptos Total: [${cryptos.length}]`
-    );
-
-    cryptos.forEach((crypto, index) => {
-      setTimeout(() => {
-        axios
-          .get(
-            `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${crypto}`,
-            {
-              headers: { 'X-CMC_PRO_API_KEY': config.apikey },
-            }
-          )
-          .then((o) => {
-            const imageUrl = o.data.data[_.upperCase(crypto)].logo;
-            const options = {
-              url: imageUrl,
-              dest: `${config.folderDist}/${crypto}.png`,
-            };
-
-            download
-              .image(options)
-              .then(({ filename }) => {
-                cmlog.success(`Image sauvegarder ${filename}`);
-                cmlog.waitting(`PROGRESS [${index}/${cryptos.length}]`);
-              })
-              .catch((err) => cmlog.error(`1 Erreur image ${err}`));
-          })
-          .catch((err) => cmlog.error(`2 Erreur image ${err}`));
-      }, 10000 * (index + 1));
-    });
-  })
-  .catch((err) => {
-    cmlog.error(new Error(err));
-  });
+} else {
+  cmlog.error(new Error('No API key entered in config.json !'));
+}
